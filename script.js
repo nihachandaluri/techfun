7f4a6c02a1183f65751a4692a4d4923373ab9eaf
nihachandaluri <niharikachn2003@gmail.com>
2022-11-16 16:33:14 +0530

    Update script.js
    added 10 quiz questions

---
+++ script.js
@@ -0,0 +1,162 @@
+const  quizDB = [
+    {
+        question: "Q1: Documents, Movies, Images and Photographs etc are stored at a?",
+        a:"application server",
+        b:"web server",
+        c:"print server",
+        d:"file server",
+        ans: "ans4"
+    },
+    {
+        question: "Q2:Technology used to provide internet by transmitting data over wires of telephone network is ?",
+        a:"transmitter",
+        b:"DSL",
+        c:"HHL",
+        d:"Diodes",
+        ans: "ans2"
+    },
+    {
+        question: "Q3:In which of the following form, data is stored in computer ?",
+        a:"Binary",
+        b:"Decimal",
+        c:"Hectadecimal",
+        d:"octadecimal",
+        ans: "ans1"
+    },
+    {
+        question: "Q4:  Where is RAM located ?",
+        a:"expansion board",
+        b:"External drive",
+        c:"Mother board",
+        d:"All the above",
+        ans: "ans3"
+    },
+    {
+        question: "Q5: Who was the founder of Bluetooth? ",
+        a:"Ecrison",
+        b:"Martin Cooper",
+        c:"Steeve",
+        d:"Apple",
+        ans: "ans1"
+    },
+    {
+        question: "Q6:AD,EH,IL,__,QT,UX?",
+        a:"MP",
+        b:"MN",
+        c:"MO",
+        d:"MQ",
+        ans: "ans1"
+    },
+    {
+        question: "Q7: mechanic:wrench::surgeon",
+        a:"probes",
+        b:"stethoscope",
+        c:"scalpel",
+        d:"x-ray",
+        ans: "ans3"
+    },
+    {
+        question: "Q8:Find the odd one out of the series 53,59,61,87",
+        a:"53",
+        b:"59",
+        c:"61",
+        d:"87",
+        ans: "ans4"
+    },
+    {
+        question: "Q9:What does LG stands for in LG Electronics",
+        a:"Lucky goldster",
+        b:"Lucky goldstar",
+        c:"Luck goldster",
+        d:"Lucky golden",
+        ans: "ans2"
+    },
+    {
+        question: "Q10:If Computer-QSTRVYUP and Science-XQZUWQU then rest-?",
+        a:"RUXY",
+        b:"RUXT",
+        c:"PUXY",
+        d:"PXYU",
+        ans: ""
+    },
+    {
+        question: "Q11:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q12:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q13:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q14:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q15:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q16:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q17:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q18:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q19:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    },
+    {
+        question: "Q20:",
+        a:"",
+        b:"",
+        c:"",
+        d:"",
+        ans: ""
+    }
+];
