15c4cb9e43a72c716183b3083bca72eae43b9845
Sekhara Pramod <32711469+V-Volte@users.noreply.github.com>
2022-11-20 16:06:08 +0530

    Fixed character movement

--- script.js
+++ script.js
@@ -1,87 +1,86 @@
-const  quizData = [
-    {
+const quizData = [{
         question: "Q1: Documents, Movies, Images and Photographs etc are stored at a?",
-        a:"application server",
-        b:"web server",
-        c:"print server",
-        d:"file server",
+        a: "application server",
+        b: "web server",
+        c: "print server",
+        d: "file server",
         correct: "d",
     },
     {
         question: "Q2:Technology used to provide internet by transmitting data over wires of telephone network is ?",
-        a:"transmitter",
-        b:"DSL",
-        c:"HHL",
-        d:"Diodes",
+        a: "transmitter",
+        b: "DSL",
+        c: "HHL",
+        d: "Diodes",
         correct: "b",
     },
     {
         question: "Q3:In which of the following form, data is stored in computer ?",
-        a:"Binary",
-        b:"Decimal",
-        c:"Hectadecimal",
-        d:"octadecimal",
+        a: "Binary",
+        b: "Decimal",
+        c: "Hectadecimal",
+        d: "octadecimal",
         correct: "a",
     },
     {
         question: "Q4:  Where is RAM located ?",
-        a:"expansion board",
-        b:"External drive",
-        c:"Mother board",
-        d:"All the above",
+        a: "expansion board",
+        b: "External drive",
+        c: "Mother board",
+        d: "All the above",
         correct: "c",
     },
     {
         question: "Q5: Who was the founder of Bluetooth? ",
-        a:"Ecrison",
-        b:"Martin Cooper",
-        c:"Steeve",
-        d:"Apple",
+        a: "Ecrison",
+        b: "Martin Cooper",
+        c: "Steeve",
+        d: "Apple",
         correct: "a",
     },
     {
         question: "Q6:AD,EH,IL,__,QT,UX?",
-        a:"MP",
-        b:"MN",
-        c:"MO",
-        d:"MQ",
+        a: "MP",
+        b: "MN",
+        c: "MO",
+        d: "MQ",
         correct: "a",
     },
     {
         question: "Q7: mechanic:wrench::surgeon",
-        a:"probes",
-        b:"stethoscope",
-        c:"scalpel",
-        d:"x-ray",
+        a: "probes",
+        b: "stethoscope",
+        c: "scalpel",
+        d: "x-ray",
         correct: "c",
     },
     {
         question: "Q8:Find the odd one out of the series 53,59,61,87",
-        a:"53",
-        b:"59",
-        c:"61",
-        d:"87",
+        a: "53",
+        b: "59",
+        c: "61",
+        d: "87",
         correct: "d",
     },
     {
         question: "Q9:What does LG stands for in LG Electronics",
-        a:"Lucky goldster",
-        b:"Lucky goldstar",
-        c:"Luck goldster",
-        d:"Lucky golden",
+        a: "Lucky goldster",
+        b: "Lucky goldstar",
+        c: "Luck goldster",
+        d: "Lucky golden",
         correct: "b",
     },
     {
         question: "Q10:If Computer-QSTRVYUP and Science-XQZUWQU then rest-?",
-        a:"RUXY",
-        b:"RUXT",
-        c:"PUXY",
-        d:"PXYU",
+        a: "RUXY",
+        b: "RUXT",
+        c: "PUXY",
+        d: "PXYU",
         correct: "c",
     },
 ];
 
-const quiz= document.getElementById('.quiz');
+const quiz = document.getElementById('.quiz');
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
@@ -91,7 +90,7 @@ const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0;
-let score=0;
+let score = 0;
 
 loadQuiz()
 
@@ -116,7 +115,7 @@ function deselectAnswers() {
 function getSelected() {
     let answer
     answerEls.forEach(answerEl => {
-        if(answerEl.checked){
+        if (answerEl.checked) {
             answer = answerEl.id
         }
     })
@@ -124,19 +123,20 @@ function getSelected() {
 }
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
-    
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct){
-            charecter = document.querySelector('.charecter');
-            charecter.classList.add('animatecharecter'); 
+
+    if (answer) {
+        if (answer == quizData[currentQuiz].correct) {
+            let charecter = document.getElementById('charecter');
+            console.log("Moved")
+            charecter.style.left = charecter.getBoundingClientRect().left + 50 + 'px';
         }
-        } 
-            currentQuiz++
+    }
+    currentQuiz++
 
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
+    if (currentQuiz < quizData.length) {
+        loadQuiz()
 
-        } 
-    
-    
+    }
+
+
 })
