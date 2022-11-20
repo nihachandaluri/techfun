dd38d0a6d10ac4f8ffad670022cfdf8795d3b0bc
nihachandaluri <niharikachn2003@gmail.com>
2022-11-20 17:33:38 +0530

    timer
    added timer

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const startingMinutes = 0;
+let time = startingMinutes * 60;
+ 
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes= Math.floor(time / 60);
+    let seconds = time % 60;
+    
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time++;
+}
+
 const quizData = [{
         question: "Q1: Documents, Movies, Images and Photographs etc are stored at a?",
         a: "application server",
